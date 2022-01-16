d36f6c18395ffb2f0e20ee658c73735c840db2f3
david <x00145452@mytudublin.ie>
2022-01-16 22:49:03 +0000

    worked on settings panel. got two dropdown buttons working for changing the trade type and distance to be filtered in the tradesmen list next

--- lib/screens/normal_user_home_page.dart
+++ lib/screens/normal_user_home_page.dart
@@ -26,6 +26,33 @@ class _NormalUserHomePageState extends State<NormalUserHomePage> {
   final Stream<QuerySnapshot> _tradesmanStream =
       FirebaseFirestore.instance.collection('tradesmen').snapshots();
 
+  final _formkey = GlobalKey<FormState>();
+
+  List<DropdownMenuItem<String>> get tradesDropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("All"), value: "All"),
+      DropdownMenuItem(child: Text("Electrician"), value: "Electrician"),
+      DropdownMenuItem(child: Text("Plumber"), value: "Plumber"),
+      DropdownMenuItem(child: Text("Mechanic"), value: "Mechanic"),
+      DropdownMenuItem(child: Text("Carpenter"), value: "Carpenter"),
+    ];
+    return menuItems;
+  }
+
+  List<DropdownMenuItem<String>> get distancesDropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("5km"), value: "5km"),
+      DropdownMenuItem(child: Text("10km"), value: "10km"),
+      DropdownMenuItem(child: Text("20km"), value: "20km"),
+      DropdownMenuItem(child: Text("40km"), value: "40km"),
+      DropdownMenuItem(child: Text("80km"), value: "80km"),
+    ];
+    return menuItems;
+  }
+
+  String? _selectedTrade = "All";
+  String _selectedDistance = "20km";
+
   late User _currentUser;
 
   @override
@@ -42,7 +69,55 @@ class _NormalUserHomePageState extends State<NormalUserHomePage> {
           builder: (context) {
             return Container(
               padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: Text('Bottom Sheet'),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Filter Trades And Distance',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DropdownButtonFormField(
+                      value: _selectedTrade,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedTrade = newValue!;
+                        });
+                      },
+                      items: tradesDropdownItems,
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    DropdownButtonFormField(
+                      value: _selectedDistance,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedDistance = newValue!;
+                        });
+                      },
+                      items: distancesDropdownItems,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Update',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        print(_selectedTrade);
+                        print(_selectedDistance);
+                      },
+                    ),
+                  ],
+                ),
+              ),
             );
           });
     }
